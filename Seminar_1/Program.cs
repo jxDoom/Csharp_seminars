1e884e2b2e9c49692f002b98785f79df43b8ac07
Alexey Amanov <chelsea.alex@list.ru>
2023-04-02 00:17:58 +0300

    added Seminar_1

---
+++ Seminar_1/Program.cs
@@ -0,0 +1,68 @@
+﻿// Написать программу, которая на вход принимает число и выдает его квадрат (число умноженное на само себя).
+
+//Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+
+
+/*Console.WriteLine("Enter number: ");
+string strNum = Console.ReadLine();
+int num = Convert.ToInt32(strNum);              //Преобразует заданное значение в 32-битовое целое число со знаком
+
+int sqr = num * num;                            //sqr - квадрат. Можно использовать: double sqrDb = Math.Pow(num,2), где 2 - возведение в степень
+                                                //                                   int num = Convert.ToInt32(strDb);
+
+System.Console.WriteLine($"sqr = {sqr}");       //$ - интерполяция строк
+*/
+
+
+/*Console.WriteLine("Enter number: ");
+string strNum1 = Console.ReadLine();
+int num1 = Convert.ToInt32(strNum1);
+
+Console.WriteLine("Enter number: ");
+string strNum2 = Console.ReadLine();
+int num2 = Convert.ToInt32(strNum2);
+
+if (num2*num2 == num1)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
+*/
+
+
+/*Console.WriteLine("Enter the number ");               //также эту задачу можно решить через else if
+string index = Console.ReadLine();
+switch (Convert.ToInt32(index)){                        //если у switch переменная типа int, то и у case должен стоять int
+    case 1:                                             //если у switch переменная типа string, то и у case должен стоять string
+    Console.WriteLine("Monday");
+        break;
+    case 2:
+    Console.WriteLine("Tuesday");
+        break;
+    case 3:
+    Console.WriteLine("Wednesday");
+        break;
+    case 4:
+    Console.WriteLine("Thursday");
+        break;
+    case 5:
+    Console.WriteLine("Friday");
+        break;
+    case 6:
+    Console.WriteLine("Saturday");
+        break;
+    case 7:
+    Console.WriteLine("Sunday");
+        break;
+    default: Console.WriteLine("Wrong number entered ");
+        break;
+}
+*/
+
+// Автоформатирование кода shift+alt+F
