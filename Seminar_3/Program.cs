6f206e7c452c5549bbaa47c3b8f8f402fb741b94
Alexey Amanov <chelsea.alex@list.ru>
2023-04-02 00:18:18 +0300

    added Seminar_3

---
+++ Seminar_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая по заданному номеру четверти,
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.WriteLine("Enter number of quter");
+    int num=int.Parse(Console.ReadLine());
+    
+    switch(num)
+    {
+        case 1:Console.WriteLine("X>0, Y>0");break;
+        case 2:Console.WriteLine("X<0, Y>0");break;
+        case 3:Console.WriteLine("X<0, Y<0");break;
+        case 4:Console.WriteLine("X>0, Y<0");break;
+        default:Console.WriteLine("Wrong quoter num");break;
+        
+    }
