39c36e97c01a240d8041df420618c41a39162d3c
Alexey Amanov <chelsea.alex@list.ru>
2023-04-04 23:59:48 +0300

    added files with functions and arrays

---
+++ Lesson/Example008_IntroMethod/Program.cs
@@ -0,0 +1,23 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 64, b1 = 12, c1 = 62,
+    a2 = 38, b2 = 86, c2 = 15,
+    a3 = 9, b3 = 14, c3 = 25;
+
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+// int max = Max(max1, max2, max3);
+
+int max = Max(
+    Max(a1, b1, c1),
+    Max(a2, b2, c2),
+    Max(a3, b3, c3));
+    
+System.Console.WriteLine(max);
---
+++ Lesson/Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,47 @@
+﻿void FillArray(int[] collection)                // Метод, который ничего не возвращает,
+                                                // то есть в этой части кода мы не будем использовать оператор return
+{
+    int lengt = collection.Length;
+    int index = 0;
+    while (index < lengt)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        System.Console.Write($"{col[position]} ");
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;                          //индекс массива от 0 до N
+    int position = -1;                      //позиция числа которую нужно найти в массиве, если этого числа нет в массиве, то возвращается -1
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+int[] array = new int[10];
+
+FillArray(array);               // заполняем массив
+PrintArray(array);              // выводим его
+System.Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+System.Console.WriteLine(pos);
