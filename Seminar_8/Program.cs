e37ee6f6b71550404e6701ee135cd2001c5faef1
Alexey Amanov <chelsea.alex@list.ru>
2023-04-24 22:46:10 +0300

    added Seminar 8 and 9

---
+++ Seminar_8/Program.cs
@@ -0,0 +1,109 @@
+﻿// Задача 53: Задайте двумерный массив. Напишите программу,
+// которая поменяет местами первую и последнюю строку массива.
+
+//Задаём метод двумерного массива
+int[,] GetRandomMatrix(int rows, int columns, int min, int max)
+{
+    int[,] array = new int[rows, columns];
+
+    var rand = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = rand.Next(min, max + 1);
+        }
+    }
+    return array;
+}
+
+//Вывод матрицы
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+//Метод замены первой строки с последней
+void StringMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            int n = matrix.GetLength(0);
+            int tmp = matrix[0, j];
+            matrix[0, j] = matrix[n - 1, j];
+            matrix[n - 1, j] = tmp;
+        }
+    }
+}
+
+void Solution_53()
+{
+    int[,] matrix = GetRandomMatrix(5, 5, 0, 9);
+    PrintMatrix(matrix);
+    Console.WriteLine();
+    StringMatrix(matrix);
+    PrintMatrix(matrix);
+}
+
+//Solution_53();
+
+
+
+// Задача 55: Задайте двумерный массив. Напишите программу,
+// которая заменяет строки на столбцы. В случае, если это невозможно,
+// программа должна вывести сообщение для пользователя.
+
+//Метод замены строк на столбцы
+int[,] RowsColumnsMatrix(int[,] matrix)
+{
+    int[,] matr = new int[matrix.GetLength(1), matrix.GetLength(0)];
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(0); j++)
+        {
+            matr[i, j] = matrix[j, i];
+        }
+    }
+    return matr;
+}
+
+bool SimmetrMatrix(int[,] matrix)
+{
+    if (matrix.GetLength(0) == matrix.GetLength(1))
+    {
+        return true;
+    }
+    else
+    {
+        Console.WriteLine("Matrix is not symmetrical");
+        return false;
+        //break;
+    }
+}
+
+void Solution_55()
+{
+    int[,] matrix = GetRandomMatrix(4, 5, 0, 9);
+    PrintMatrix(matrix);
+    SimmetrMatrix(matrix);
+    //for (; ; ) if (false) break;
+    Console.WriteLine();
+    int[,] newMatrix = RowsColumnsMatrix(matrix);
+    PrintMatrix(newMatrix);
+}
+
+//Solution_55();
+
+
+// Задача 57: Составить частотный словарь элементов двумерного массива.
+// Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.
+
---
+++ Seminar_9/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача ***. Задать 10 вопросов на каждом из которых будут 4 варианта ответа, один из них будет правильный
+/*
+string[,] QuestionsArray()
+{
+    string[,] matrix = {{"2+2 = ?", "13", "2", "4", "5", "4"},
+    {"А и Б сидели на трубе, что с ними стало", "Упали", "Пропали", "Сбежали", "Потерялись", "Сбежали"}
+    // {"", "", "", "", "", ""},
+    // {"", "", "", "", "", ""},
+    // {"", "", "", "", "", ""},
+    // {"", "", "", "", "", ""},
+    // {"", "", "", "", "", ""},
+    // {"", "", "", "", "", ""},
+    };
+    return matrix;
+}
+
+string[,] questions = QuestionsArray();
+int count = 0;
+for (int i = 0; i < questions.GetLength(0); i++)
+{
+    Console.WriteLine(questions[i, 0]);
+
+    for (int j = 1; j < questions.GetLength(1) - 1; j++)
+    {
+        Console.WriteLine(questions[i, j]);
+    }
+    Console.WriteLine();
+
+
+    string answer = Console.ReadLine();
+    if (answer == questions[i, questions.GetLength(1) - 1])
+    {
+        count++;
+    }
+
+    Console.WriteLine($"Correct answers: {count}");
+}
+*/
+
+// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+//Ввод числа с клавиатуры
+int SetNumber(string numberName)
+{
+    Console.Write($"Enter number {numberName}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int SumAllDigits(int num)
+{
+    // if (num == 0) return 0;
+    // return (num % 10 + SumAllDigits(num / 10));
+    return (num == 0) ? 0 : (num % 10 + SumAllDigits(num / 10));
+}
+int number = SetNumber("");
+Console.WriteLine($"Sum numbers {number} equals {SumAllDigits(number)}");
